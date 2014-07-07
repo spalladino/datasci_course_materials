f4ff342f1a4d6dacf35938ec36856a0c38afdc34
Santiago Palladino <spalladino@gmail.com>
2014-07-06 22:21:43 -0300

    Tweet sentiment file

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,31 @@
 import sys
+import json
+import re
 
-def hw():
-    print 'Hello, world!'
+def load_scores(fp):
+  scores = {}
+  for line in fp:
+    term, score  = line.split("\t")
+    scores[term] = int(score)
+  return scores
 
-def lines(fp):
-    print str(len(fp.readlines()))
+def split_words(text):
+  return re.split("\W+", text)
+
+def sentiment(scores, words):
+  return sum([scores.get(word.lower(), 0) for word in words])
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+  scores = None
+  with open(sys.argv[1]) as sent_file:
+    scores = load_scores(sent_file)
+
+  with open(sys.argv[2]) as tweet_file:
+    for tweet_json in tweet_file:
+      tweet = json.loads(tweet_json)
+      text = tweet['text']
+      words = split_words(text)
+      print sentiment(scores, words), text
 
 if __name__ == '__main__':
-    main()
+  main()
